54cbfc9d2792cbb436675034e780755f6d47c0bf
Abhishek <you@example.com>
2022-08-21 09:03:27 +0530

    [Abhishek] Added: 1.Stock Account Management Program

---
+++ src/com/bridgelabz/objectOriented/StockMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.objectOriented;
+
+public class StockMain {
+    public static void welcome() {
+        System.out.println("Welcome to Stock Account Management Program");
+    }
+    public static void main(String[] args){
+        welcome();
+    }
+}
